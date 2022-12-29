9bc23885c8dd7073df618b5e206f607f5079e55d
Anthony Moises Condori Choccata <113246456+Alt-Anthony@users.noreply.github.com>
2022-12-29 17:43:35 -0500

    Commit arreglar

--- ApiPagos/settings.py
+++ ApiPagos/settings.py
@@ -44,11 +44,13 @@ INSTALLED_APPS = [
     'django_filters',
     'drf_yasg',
     'rest_framework_simplejwt',
+    'corsheaders'
 
 ]
 
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -153,3 +155,11 @@ REST_FRAMEWORK = {
         'all':"10/second",
     }
 }
+
+CORS_ALLOWED_ORIGINS = [
+    'http://127.0.0.1:5500',    
+]
+
+CORS = [
+    
+]
--- api_usuarios/views.py
+++ api_usuarios/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from rest_framework import generics
 from .serializers import UserSerializer
 from django.contrib.auth.models import User
+from django.shortcuts import get_object_or_404
+from rest_framework.decorators import api_view
+from django.contrib.auth import get_user_model
 
 class UserCreateView(generics.CreateAPIView):
     serializer_class = UserSerializer
@@ -9,3 +12,9 @@ class UserCreateView(generics.CreateAPIView):
 class UserListView(generics.ListAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+
+@api_view(['GET'])
+def get_user(request, username):
+    User = get_user_model()
+    User = get_object_or_404(User, username=username)
